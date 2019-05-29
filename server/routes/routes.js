81e5c1cf793870ef9942ab357029efee3be03e4f
Jesper Andreas Olsen <325598@RTS-365.dk>
2019-05-29 11:14:32 +0200

    Started on Single Article Page and Updated routes.js

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -5,9 +5,15 @@ module.exports = (app) => {
          title: "The News Paper - News & Lifestyle Magazine Template"
       });
    });
-   
+
    app.get('/categories-post', (req, res, next) => {
       res.render('categories-post', {
+         title: "The News Paper - News & Lifestyle Magazine Template"
+      });
+   });
+
+   app.get('/single-post', (req, res, next) => {
+      res.render('single-post', {
          title: "The News Paper - News & Lifestyle Magazine Template"
       });
    });
